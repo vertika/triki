6842a3018e47d347a9b7bbdbb2d0b948f9e7a06e
Mark Mevorah <mmevorah@umich.edu>
2014-04-07 23:19:35 -0400

    Added Wiki Traversal Script and Integrated It Into Game
    Still a lot to be done, but so far a decent place holder for the page.

---
+++ js/WikiTravScript.js
@@ -0,0 +1,189 @@
+//Individual question and answer
+function QandA(){
+	this.category = "";
+	this.url = "en.wikipedia.org/wiki/";
+	this.question = "";
+	this.correctAnswer = "";
+	this.incorrect1 = "";
+	this.incorrect2 = "";
+	this.incorrect3 = "";
+}
+
+//Wiki Traverser
+function WikiTraverser(){
+}
+
+WikiTraverser.prototype.numQuestionsGeneratedSoFar = 0;
+WikiTraverser.prototype.qAndAs = [];
+
+WikiTraverser.prototype.collectForCategory = function(category, trav){
+	var toPopulate = [];
+	this.traverseCategory(category, toPopulate, trav);
+}
+
+//Takes a starting category and traverses down a path
+//of random categories. On every child category, all of the
+//pageid's are collected and put into an array resembling:
+//Array< Array<pageId> > where the inner array represents
+//a category from which those pageIds were found. In generating
+//question and answers, it makes sense that those towards the end
+//of the array, ie further away from the base category, will be more
+//difficult to answer than those general questions found at the beginning
+//closer to the base category.
+WikiTraverser.prototype.traverseCategory = function(nextCategory, toPopulate, trav){
+	//Create url, ie the actual search
+	var urlC = 'http://en.wikipedia.org/w/api.php?' +
+	'callback=?' +
+	'&format=json' +
+	'&action=query' +
+	'&list=categorymembers' +
+	'&cmlimit=100' +
+	'&cmtitle=' + nextCategory +
+	'&cmtype=subcat|page';
+
+
+	//Perform query
+	$.ajax({
+		url: urlC,
+		dataType: 'json',
+		async: false,
+		success: function(data){
+			var json_data = JSON.stringify(data);
+			var wikiData = jQuery.parseJSON(json_data);
+			var categoryMembers = wikiData.query.categorymembers;
+
+			var pages = [];
+			var categories = [];
+
+			//Right here you have all of the subcategories and
+			//pages within a category.
+
+			categoryMembers.forEach(function(entry){
+				//determine if entry is a category or page
+				var split = entry.title.split(":");
+				if(split[0] === "Category"){
+					categories.push("Category:" + split[1]);
+				}else{
+					//Pushing pageids
+					//May want to consider page names,
+					//really just depends on the easiest
+					//way to search and pull up an article.
+					//title for page title
+					pages.push(entry.title);
+				}
+			});
+			//	console.log("Categories:" + categories);
+			//	console.log("Pages:" + pages);
+			toPopulate.push(pages);
+
+			if(categories.length > 0){
+				var catIndex = Math.floor(Math.random() * categories.length);
+				var	nextCategory = categories[catIndex];
+			//	console.log("Next Category to search:" + nextCategory);
+				WikiTraverser.prototype.traverseCategory(nextCategory, toPopulate, trav);
+			}else{
+				WikiTraverser.prototype.populateQuestions(toPopulate, trav);
+			}
+		}
+	});
+}
+
+
+//Because AJAX works asynchronously, a timeout must be set
+//in order for the function to have enough time to populate
+//the array.
+
+//Consider changing to a polling function that checks if the array has returned
+//data;
+WikiTraverser.prototype.populateQuestions = function(toPopulate, trav){
+	console.log(toPopulate);
+	var groupsOfFour = [];
+
+	var numAnswers = 0;
+	toPopulate.forEach(function(arr){
+		var arrayLength = arr.length;
+		numAnswers += arrayLength;
+	});
+
+	if(numAnswers >= 20){
+		var total = 0;
+		for(var i = toPopulate.length - 1; i >= 0; i--){
+			var cat = toPopulate[i];
+			while(cat.length >= 4){
+				var group = [];
+				for(var j = 0; j < 4; j++){
+					var index =  Math.floor(Math.random() * cat.length);
+					var page = cat[index];
+					group.push(page);
+					cat.splice(index, 1);
+				}
+				groupsOfFour.push(group);
+			}
+		}
+	}
+
+	//Generate
+	for(var r = 0; r < 5; r++){
+		this.generateQandA(groupsOfFour[r], trav);
+	}
+},
+
+WikiTraverser.prototype.generateQandA = function(groupOfFour, trav){
+	console.log(groupOfFour);
+	var url = 'http://en.wikipedia.org/w/api.php?' +
+	'callback=?' +
+	'&format=json' +
+	'&action=query' +
+	'&prop=extracts' +
+	'&exsectionformat=plain' +
+	'&exsentences=2' +
+	'&titles=' + groupOfFour[0] + '|' +
+	groupOfFour[1] + '|' +
+	groupOfFour[2] + '|' +
+	groupOfFour[3];
+	$.getJSON(url, function(data){
+		var json_data = JSON.stringify(data);
+		var wikiData = jQuery.parseJSON(json_data);
+		var question = [];
+		$.each(wikiData.query.pages, function(key, value){
+			//console.log(value);
+			question.push(value);
+		});
+
+		//In parsing, use try catch block
+		var item = new QandA();
+		//Generate URL
+		question.forEach(function(entry){
+			item.category = trav.category;
+			if(entry.extract != undefined){
+
+				item.correctAnswer = entry.title;
+				var rawQuestion = entry.extract;
+
+				var html = rawQuestion;
+				var div = document.createElement("div");
+				div.innerHTML = html;
+				var text = div.textContent || div.innerText || "";
+				var question = text.replace(item.correctAnswer, "______", "gi");
+
+				item.question  = question;
+
+			}else if(item.incorrect1 == ""){
+				item.incorrect1 = entry.title;
+			}else if(item.incorrect2 == ""){
+				item.incorrect2 = entry.title;
+			}else if(item.incorrect3 == ""){
+				item.incorrect3 = entry.title;
+			}else{
+				console.log("Error: Something Wrong in GenerateQandA");
+			}
+		});
+		var title = item.correctAnswer;
+		item.url += title.replace(/ /g, "_");
+
+		WikiTraverser.prototype.qAndAs.push(item);
+
+		WikiTraverser.prototype.numQuestionsGeneratedSoFar++;
+		
+	});
+}
--- js/gamePage.js
+++ js/gamePage.js
@@ -2,25 +2,69 @@ var gameCountdownTime = 5;
 var paused = false;
 /*------ Init Page -----*/
 
+//Get Categories
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var cat1 = getParameterByName('Category1');
+var cat2 = getParameterByName('Category2');
+var cat3 = getParameterByName('Category3');
+var cat4 = getParameterByName('Category4');
+var cat5 = getParameterByName('Category5');
+var cat6 = getParameterByName('Category6');
+
+//Set Category Titles
+
 //Open Modal Countdown Window
 $(function() {
+  $('#numQuestionsGenerated').text("0/30 Questions Generated");
   $('#countdown-modal').modal({
     backdrop: 'static'
   });
 });
 
-//Do Timer and Countdown Business
+/*----- Generate Questions and Place In Gameboard ------*/
+var cats = ["Category:"+cat1,
+            "Category:"+cat2,
+            "Category:"+cat3,
+            "Category:"+cat4,
+            "Category:"+cat5,
+            "Category:"+cat6];
+generateQuestions(cats);
+function generateQuestions(categories){
+  categories.forEach(function(category){
+    var trav = new WikiTraverser();
+    trav.category = category;
+    trav.collectForCategory(category, trav);
+  });
+}
+
+/*----- Do Timer and Countdown Business -----*/
 initialTimer = window.setInterval(startCountDownToGame, 1000);
 
 function startCountDownToGame(){
   gameCountdownTime -= 1;
   $('#gameCountdown').text(gameCountdownTime);
 
+  $('#numQuestionsGenerated').text(WikiTraverser.prototype.numQuestionsGeneratedSoFar + "/30 Questions Generated");
+
   if(gameCountdownTime === 0){
+    //Stop Clock
     clearInterval(initialTimer);
 
-    //start game
+    //Dismiss Modal
     $('#countdown-modal').modal('hide');
+
+    //If not enough questions reload and try again
+    if(WikiTraverser.prototype.numQuestionsGeneratedSoFar != 30){
+      location.reload();
+    }
+
+    //Start Game
 
   }
 }
@@ -38,11 +82,27 @@ function togglePause(){
   }
 }
 
-
-
-
-
 /*----- Game -----*/
+
+function questionSelected(questionNumber){
+  $("#question-modal").modal({
+    backdrop: 'static'
+  });
+
+  var qanda = WikiTraverser.prototype.qAndAs[questionNumber];
+
+  $("#questionText").text(qanda.question);
+
+  //do random
+  //Also make these buttons
+  //Also ensure that the order is correct
+  //Also add categories to the top
+  $("#option1").text(qanda.correctAnswer);
+  $("#option2").text(qanda.incorrect1);
+  $("#option3").text(qanda.incorrect2);
+  $("#option4").text(qanda.incorrect3);
+
+}
 
 //Whole business with opening up dialog
 //Determine if correct answer selected
