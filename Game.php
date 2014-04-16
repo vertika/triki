7575f75217d0c05a567cc13826f68593caf731e6
Mark Mevorah <mmevorah@umich.edu>
2014-04-16 02:15:20 -0400

    Updated Results Page

--- Game.php
+++ Game.php
@@ -1,3 +1,16 @@
+<?php
+
+	session_start();
+
+	require "fb-sdk/facebook.php";
+
+	$facebook = new Facebook(array(
+		'appId'		=> '578241305624655',
+		'secret' 	=> '163708fdec7503fe860750419670152a',
+	));
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
