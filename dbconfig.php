ffa282542debaeb90fb519127b51c181f7bf4b06
redhat <roychou@umich.edu>
2014-04-17 11:40:50 -0400

    db test

--- dbconfig.php
+++ dbconfig.php
@@ -1,9 +1,15 @@
 <?php
-	define ("DB_HOST", "www.rzchou.com");
-	define ("DB_USER", "roy");
-	define ("DB_PASSWD", "chou");
-	define ("DB_NAME", "test");
+	// define ("DB_HOST", "www.rzchou.com");
+	// define ("DB_USER", "roy");
+	// define ("DB_PASSWD", "chou");
+	// define ("DB_NAME", "test");
 
-	$link = mysql_connect (DB_HOST, DB_USER, DB_PASSWD) or die ("Couldn't make connection.");
-	$db = mysql_select_db (DB_NAME, $link) or die ("Couldn't select database");
+	$mysql_hostname = "www.rzchou.com";
+	$mysql_user = "roy";
+	$mysql_password = "chou";
+	$mysql_database = "test";
+	$link = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
+
+	// $link = mysql_connect (DB_HOST, DB_USER, DB_PASSWD) or die ("Couldn't make connection.");
+	mysql_select_db (mysql_database, $link) or die ("Could not select database");
 ?>
