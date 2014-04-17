f91223d18090f6d12fbb085884272e7b3121d96d
redhat <roychou@umich.edu>
2014-04-17 11:36:11 -0400

    db test

--- Landing.php
+++ Landing.php
@@ -3,7 +3,7 @@
 	session_start();
 
 	require "fb-sdk/facebook.php";
-
+	include "dbconfig.php";
 	$facebook = new Facebook(array(
 		'appId'		=> '578241305624655',
 		'secret' 	=> '163708fdec7503fe860750419670152a',
@@ -61,7 +61,7 @@
 				</div>
 
 				<div id="alternateLogin" align="center">
-					<?php include "dbconfig.php";
+					<?php
 						$msg = "";
 						if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 						    $name = $_POST["name"];
