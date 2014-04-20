ea904c14f9e2cb9ca3bde2b43628390b165e5b51
redhat <roychou@umich.edu>
2014-04-20 11:41:25 -0400

    roy alt login test

--- Landing.php
+++ Landing.php
@@ -88,8 +88,8 @@
 					<h4>Why Sign Up? </h4>
 					<p>1. You will be able to keep track of your scores</p>
 					<p>2. You can view your most played categories</p>
-					<form action="register.php">
-						<class="btn btn-success" input type="submit" value="Sign Up!">
+					<form action="register.php" >
+						<input type="submit" value="Sign Up!" class="btn btn-success">
 					</form>
 					<!-- <button type="button" class="btn btn-success">Sign up<a href="register.php"></a></button> -->
 				</div>
