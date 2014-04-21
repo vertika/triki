19e0cbbf2d2450fadec6f441a357188d3b2ab7b3
Mark Mevorah <mmevorah@umich.edu>
2014-04-21 13:53:57 -0400

    edited logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,13 @@
 <?php
 
-
+/*
 	require 'fb-sdk/facebook.php';
 
 	$facebook = new Facebook(array(
   		'appId'  => '578241305624655',
   		'secret' => '163708fdec7503fe860750419670152a',
-	));
- 
+	));*/
+
  	// echo "BLA";
 	//on logout page
 	// $params = array('next' => $logout.php');
@@ -15,8 +15,7 @@
 
 	setcookie('fbs_'.$facebook->getAppId(), '', time()-100, '/', 'rzchou.com');
 	session_destroy();
-	$facebook->destroySession();
-	// header('Location: http://www.rzchou.com/Landing.php');
+//	$facebook->destroySession();
 	header('Location: /');
 
 ?>
