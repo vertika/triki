ea3c3b7a70eae041242694f273f9789059c86a8d
redhat <roychou@umich.edu>
2014-04-21 14:52:35 -0400

    home page

--- Home.php
+++ Home.php
@@ -14,6 +14,7 @@
 				WHERE username = '$username';";
 		$result = mysqli_query($db, $query);
 		$gameData = mysqli_fetch_array($result, MYSQL_ASSOC);
+		echo $gameData["gamesplayed"];
 
 	}
 	else {
