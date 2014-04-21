c759a80d8461693b9e754769f2d1a9bd631237c7
redhat <roychou@umich.edu>
2014-04-21 00:30:07 -0400

    php sql stuff

--- pushResults.php
+++ pushResults.php
@@ -22,7 +22,7 @@
     $cat6 = strtolower($cat6);
 
     $query = "UPDATE gamestats SET gamesplayed = gamesplayed + 1 WHERE username = '$username'; ";
-    $query .= "UPDATE gamestats SET numcorrect = numcorrect + . $numcorrect . WHERE username = '$username'; ";
+    $query .= "UPDATE gamestats SET numcorrect = numcorrect + $numcorrect WHERE username = '$username'; ";
     $query .= "UPDATE gamestats SET $cat1 = $cat1 + 1 WHERE username = '$username'; ";
     echo $query;
     $query .= "UPDATE gamestats SET $cat2 = $cat2 + 1 WHERE username = '$username'; ";
