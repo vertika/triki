3a7e0649f6d5fe800e7e92096b1ebb2dd9e3db98
redhat <roychou@umich.edu>
2014-04-21 14:24:03 -0400

    bla

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 	$mysql_hostname = "localhost";
-	$mysql_user = "roy";
-	$mysql_password = "chou";
-	$mysql_database = "test";
+	$mysql_user = "trikiadmin";
+	$mysql_password = "uaf";
+	$mysql_database = "triki";
 	$db = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
 
 	if (mysqli_connect_errno()){
